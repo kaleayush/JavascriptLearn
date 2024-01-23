ba2db8ee5bd480145be18480a3017acca4fd6ddf
kaleayush <118829503+kaleayush@users.noreply.github.com>
2024-01-23 14:01:52 +0000

    Date.js file added

---
+++ .devcontainer/Basis/07_Date.js
@@ -0,0 +1,9 @@
+let myDate=new Date()
+// console.log(myDate.getDate())
+// console.log(myDate.getDay())
+// console.log(myDate.toDateString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toLocaleTimeString())  
+
+let myCreatedDate=new Date(2024,0,22)
+console.log(myCreatedDate.toDateString())
