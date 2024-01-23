db7da0152d42468167c74f900fb8a6b47a2b087e
kaleayush <118829503+kaleayush@users.noreply.github.com>
2024-01-23 13:19:53 +0000

    NumsAndMaths.js file added

---
+++ .devcontainer/Basis/NumsAndMaths.js
@@ -0,0 +1,33 @@
+const score = 400
+//console.log(typeof(score));
+
+
+// let num=new Number(100)
+// console.log(num)
+// console.log(num.toExponential(2))
+// console.log(num.toFixed(3))
+// console.log(num.toString().length)
+// console.log(num.valueOf());
+// console.log(new Number())
+
+// const num1=23.666
+// console.log(num1.toPrecision(2))
+// const rupess=1000000
+// console.log(rupess.toLocaleString())
+// console.log(rupess.toLocaleString('en-In'))
+// console.log(rupess)
+
+//######################### MATHS ############################
+
+// console.log(Math.PI);
+// console.log(Math.SQRT2)
+// console.log(Math.LN10)
+// console.log(Math.abs(-44))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.5))
+// console.log(Math.round(2.4))
+console.log(Math.random())
+console.log((Math.random()*10) +1 )
+const min =1
+const max= 6
+console.log(Math.floor(Math.random() * (max-min+1))+min)
---
+++ .devcontainer/Basis/stringdemo.js
@@ -0,0 +1,11 @@
+let str="hellow"
+let str1="world"
+console.log(str.length);
+console.log(str.charAt(2));
+console.log(str.concat('').concat(str1));
+console.log(str.endsWith());
+console.log(str.includes("he"))
+console.log(str.indexOf('o'))
+console.log(str.split())
+console.log(str.toLocaleLowerCase())
+console.log(str.bold())
